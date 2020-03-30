4afce796096e79805414201257a00e60424b4c29
Mason Balbera <mhbalbera@gmail.com>
2020-03-30 11:28:25 -0400

    set up favorites

--- dropBy-frontend/screens/FavoritesScreen.js
+++ dropBy-frontend/screens/FavoritesScreen.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 
 export default function FavoritesScreen() {
     return (
-        <View>
-            <Text>
-                FavoritesScreen
-           </Text>
+        <View style={{ backgroundColor: 'rgb(255,246,222)', flex: 1 }}>
+            <View style={{ height: '20%', paddingTop: '3%', justifyContent: 'space-around' }}>
+                <Text style={{ margin: 'auto', fontSize: 26, textAlign: 'center' }}>Your Favorites</Text>
+            </View>
+            <ScrollView style={{ height: '5%', alignItems: 'center', justifyContent: 'center', flex: 1 }}>
+               <Text>List Item</Text>
+               <Text>List Item</Text>
+               <Text>List Item</Text>
+               <Text>List Item</Text>
+               <Text>List Item</Text>
+               <Text>List Item</Text>
+               <Text>List Item</Text>
+               <Text>List Item</Text>
+            </ScrollView>
         </View>
     );
 }
